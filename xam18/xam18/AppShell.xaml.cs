ab894fe553df7fa2873b06b1c6f1410f5da97c8a
Ivan Janečka <ivanj9@gmail.com>
2020-04-10 12:15:46 +0200

    Add project files.

---
+++ xam18/xam18/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace xam18
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            FlyoutIsPresented = false;
+            // Shell.Current.FlyoutIsPresented = false;
+            DisplayAlert("Alert","Message to show", "OK");
+
+        }
+    }
+}
